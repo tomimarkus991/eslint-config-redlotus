1893d24a3999b491af6c7ba90ccc0055bc3ca1fb
tomimarkus991 <tomimarkusalber@gmail.com>
2022-10-06 14:01:42 +0300

    Add config

---
+++ src/api.js
@@ -0,0 +1,1 @@
+module.exports = require('../rules/api.yaml');
--- src/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: {
-    api: require("./rules/api.yaml"),
-    ui: require("./rules/ui.yaml"),
-  },
-};
