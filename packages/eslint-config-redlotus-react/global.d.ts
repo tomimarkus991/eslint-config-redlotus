988954c1c3ab975c05888e6dd831cc1870ffbe41
tomimarkus991 <tomimarkusalber@gmail.com>
2022-10-26 10:01:25 +0300

    Try to fix react config

--- packages/eslint-config-redlotus-react/global.d.ts
+++ packages/eslint-config-redlotus-react/global.d.ts
@@ -1,3 +1,4 @@
 declare module 'eslint-config-redlotus-base';
 declare module 'eslint-plugin-react';
 declare module 'eslint-plugin-react-hooks';
+declare module 'eslint-plugin-tailwindcss';
--- packages/eslint-config-redlotus-react/main.ts
+++ packages/eslint-config-redlotus-react/main.ts
@@ -1,9 +1,9 @@
-module.exports = {
-  // @ts-ignore
-  extends: ["eslint-config-redlotus-base/dist/main", "./rules/react"].map(require.resolve),
-  rules: {},
-};
-
 export * from "eslint-config-redlotus-base";
 export * from "eslint-plugin-react";
 export * from "eslint-plugin-react-hooks";
+export * as sus from "eslint-plugin-tailwindcss";
+
+module.exports = {
+  // @ts-ignore
+  extends: ["eslint-config-redlotus-base/dist/main", "./rules/react"].map(require.resolve),
+};
