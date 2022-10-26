2ff59dd473c46a5b25cdad5d11ce3e5c7de709a1
tomimarkus991 <tomimarkusalber@gmail.com>
2022-10-26 10:16:39 +0300

    Implement new prettier export

--- packages/eslint-config-redlotus-base/main.ts
+++ packages/eslint-config-redlotus-base/main.ts
@@ -1,6 +1,6 @@
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginImport from "eslint-plugin-import";
-
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export * from "@typescript-eslint/eslint-plugin";
 export * from "@typescript-eslint/parser";
@@ -8,11 +8,10 @@ export * from "eslint";
 export * from "eslint-import-resolver-typescript";
 export * from "eslint-plugin-jest-dom";
 export * from "eslint-plugin-jsx-a11y";
-export * from "eslint-plugin-prettier";
 export * from "prettier";
 export * from "eslint-plugin-testing-library";
 
-export {eslintConfigPrettier,eslintPluginImport};
+export {eslintConfigPrettier,eslintPluginImport,eslintPluginPrettier};
 
 module.exports = {
   extends: [
