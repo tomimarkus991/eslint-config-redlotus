a7f1de800cd13e8fedf6231cf489fd0ead7951af
tomimarkus991 <tomimarkusalber@gmail.com>
2023-07-11 09:52:10 +0300

    Fix exports

--- packages/eslint-config-redlotus-base/main.ts
+++ packages/eslint-config-redlotus-base/main.ts
@@ -1,17 +1,18 @@
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginImport from "eslint-plugin-import";
 import eslintPluginPrettier from "eslint-plugin-prettier";
+// @ts-ignore
+import typescriptEslintParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
 
 export * from "eslint";
 export * from "prettier";
-export * from "@typescript-eslint/eslint-plugin";
-export * from "@typescript-eslint/parser";
 export * from "eslint-import-resolver-typescript";
 export * from "eslint-plugin-jest-dom";
 export * from "eslint-plugin-jsx-a11y";
 export * from "eslint-plugin-testing-library";
 
-export {eslintConfigPrettier,eslintPluginImport,eslintPluginPrettier};
+export {eslintConfigPrettier,eslintPluginImport,eslintPluginPrettier,typescriptEslintParser,typescriptEslintPlugin};
 
 module.exports = {
   extends: [
